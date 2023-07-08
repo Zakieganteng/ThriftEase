46293b83fa7e312c69775c2dac46ca7c78935158
Zakieganteng <zakieirdanaa@gmail.com>
2023-07-08 16:17:58 +0700

    profile

---
+++ src/Profile/profilecontroller.java
@@ -0,0 +1,22 @@
+package Profile;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+public class profilecontroller implements Initializable{
+    
+    @FXML
+    private TextField tampilnama;
+
+    @FXML
+    private TextField tampilemail;
+
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+}
